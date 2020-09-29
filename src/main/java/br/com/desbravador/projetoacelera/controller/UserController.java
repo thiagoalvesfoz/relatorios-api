2a4fe23497393d68652adae82195928debb2b6e7
Edson Pereira <edinho.pereira95@gmail.com>
2020-09-29 10:40:12 -0300

    adicinado o JPA, H2DataBase, DevTools

--- src/main/java/br/com/desbravador/projetoacelera/controller/UserController.java
+++ src/main/java/br/com/desbravador/projetoacelera/controller/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping
+@RequestMapping("api/users")
 @RestController
 
 public class UserController {
