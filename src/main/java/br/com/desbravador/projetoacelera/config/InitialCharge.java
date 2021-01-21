3ac55f5b97ca1cef8c7346ac7dd5ee4c6a970e28
Thiago Alves <thiago_marketingdigital@hotmail.com>
2021-01-21 13:08:34 -0300

    add admin on profile dev

--- src/main/java/br/com/desbravador/projetoacelera/config/InitialCharge.java
+++ src/main/java/br/com/desbravador/projetoacelera/config/InitialCharge.java
@@ -4,42 +4,51 @@ import br.com.desbravador.projetoacelera.users.domain.User;
 import br.com.desbravador.projetoacelera.users.domain.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 @Component
-@Profile("test")
 public class InitialCharge implements ApplicationListener<ContextRefreshedEvent> {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final Environment env;
 
     @Autowired
-    public InitialCharge(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public InitialCharge(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder, Environment env) {
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.env = env;
     }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent e) {
 
-        User admin = new User();
-        admin.setName("Admin");
-        admin.setEmail("admin@desbravador.com");
-        admin.setPassword(bCryptPasswordEncoder.encode("123456"));
-        admin.setAdmin(true);
-        admin.setActive(true);
-        userRepository.save(admin);
+        var admins = userRepository.findAll().stream().filter(User::isAdmin).collect(Collectors.toList());
 
-        User user = new User();
-        user.setName("Usuário");
-        user.setEmail("user@desbravador.com");
-        user.setPassword(bCryptPasswordEncoder.encode("123456"));
-        user.setAdmin(false);
-        user.setActive(true);
-        userRepository.save(user);
+        if (admins.isEmpty()) {
+            User admin = new User();
+            admin.setName("Admin");
+            admin.setEmail("admin@desbravador.com");
+            admin.setPassword(bCryptPasswordEncoder.encode("123456"));
+            admin.setAdmin(true);
+            admin.setActive(true);
+            userRepository.save(admin);
+        }
 
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            User user = new User();
+            user.setName("User");
+            user.setEmail("user@desbravador.com");
+            user.setPassword(bCryptPasswordEncoder.encode("123456"));
+            user.setAdmin(false);
+            user.setActive(true);
+            userRepository.save(user);
+        }
     }
 }
